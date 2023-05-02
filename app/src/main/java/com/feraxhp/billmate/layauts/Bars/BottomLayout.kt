74f2833f9540f6e49bcc97f0dc775246faaf71b7
feraxhp <ferax.hp@gmail.com>
2023-05-01 22:58:16 -0500

    Agregado de barra superior

--- app/src/main/java/com/feraxhp/billmate/layauts/Bars/BottomLayout.kt
+++ app/src/main/java/com/feraxhp/billmate/layauts/Bars/BottomLayout.kt
@@ -6,11 +6,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 
-@Composable
-fun TopLayout() {
-
-}
-
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BottomLayout(Names: List<String>,
--- app/src/main/java/com/feraxhp/billmate/layauts/screens/primaryUi.kt
+++ app/src/main/java/com/feraxhp/billmate/layauts/screens/primaryUi.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.feraxhp.billmate.layauts.TopLayout
 import com.feraxhp.billmate.ui.theme.BillmateTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,6 +28,9 @@ fun PrimaryUi() {
 //            color = MaterialTheme.colorScheme.primaryContainer
         ) {
             Scaffold(
+                topBar = {
+                    TopLayout()
+                },
                 bottomBar = {
                     BottomLayout(
                         Names = listOf("Home", "Accounts", "Debts", "Overview"),
