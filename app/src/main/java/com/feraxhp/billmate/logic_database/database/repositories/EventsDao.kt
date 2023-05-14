7ac87be66107281c929d4254981b78a671c1c750
Feraxhp <ferax.hp@gmail.com>
2023-05-14 12:30:15 -0500

    Metodos en EventsDao

--- app/src/main/java/com/feraxhp/billmate/logic_database/database/repositories/EventsDao.kt
+++ app/src/main/java/com/feraxhp/billmate/logic_database/database/repositories/EventsDao.kt
@@ -1,24 +1,28 @@
 package com.feraxhp.billmate.logic_database.database.repositories
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.feraxhp.billmate.logic_database.database.entities.Events
 
 @Dao
 interface EventsDao {
-//    @Query("SELECT * FROM funds")
-//    suspend fun getAllFunds(): List<Events>
-//
-//    @Query("SELECT * FROM funds WHERE id = :id")
-//    suspend fun getFundById(id: Long): Events
-//
-//    @Insert
-//    suspend fun insertFund(fund: Events)
-//
-//    @Insert
-//    suspend fun insertFunds(funds: List<Events>)
-//
-//    @Update
-//    suspend fun updateFund(fund: Events)
-//
-//    @Query("DELETE FROM funds WHERE id = :id")
-//    suspend fun removeFund(id: Long)
+    @Query("SELECT * FROM Events")
+    suspend fun getAllEvents(): List<Events>
+
+    @Query("SELECT * FROM Events WHERE id = :id")
+    suspend fun getEventById(id: Long): Events
+
+    @Insert
+    suspend fun insertEvent(Event: Events)
+
+    @Insert
+    suspend fun insertEvents(Events: List<Events>)
+
+    @Update
+    suspend fun updateEvent(Event: Events)
+
+    @Query("DELETE FROM Events WHERE id = :id")
+    suspend fun removeEvent(id: Long)
 }
