3f2b0aef00b09941c5a3d8878c15fe55c0e5d926
Feraxhp <ferax.hp@gmail.com>
2023-07-14 13:25:30 -0500

    Betta - 0.10.4: Mejora en el SegmentedButtons

--- app/src/main/java/com/feraxhp/billmate/layauts/tabs/components/components/SegmentedButtons.kt
+++ app/src/main/java/com/feraxhp/billmate/layauts/tabs/components/components/SegmentedButtons.kt
@@ -2,17 +2,16 @@ package com.feraxhp.billmate.layauts.tabs.components.components
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
@@ -22,117 +21,124 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun SegmentedButtons(
     modifier: Modifier = Modifier,
-    values: List<String> = listOf("Selected", "Enable", "Enable"),
-    selectedValue: Int = 1,
+    values: List<String> = listOf("Selected", "Enable", "Enable", "Disabled"),
+    selectedValue: Int = 0,
     setSelectedValue: (Int) -> Unit = {},
+    disabled: List<Boolean> = listOf(false, false, false, true),
 ) {
+    val height: Dp = 50.dp
+    val borderSize: Dp = 1.dp
+    val borderMediumSize: Dp = 0.dp
+
+    val enableColor = MaterialTheme.colorScheme.onSurface
+    val disableColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+
     Row(
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        modifier = modifier
+        modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp)
-            .background(
-                shape = RoundedCornerShape(50.dp),
-                color = MaterialTheme.colorScheme.surface
-            )
-            .border(
-                width = .5.dp,
-                color = MaterialTheme.colorScheme.onSurface,
-                shape = RoundedCornerShape(50.dp)
-            )
+            .height(height)
+            .clip(RoundedCornerShape(50))
+            .background(MaterialTheme.colorScheme.surface)
+            .then(modifier)
     ) {
         for (index in values.indices) {
-            val onColor: Color = when (index) {
-                selectedValue -> MaterialTheme.colorScheme.onSecondaryContainer
-                else -> MaterialTheme.colorScheme.onSurface
-            }
-            if (index != 0) {
-                Box(
-                    modifier = Modifier
-                        .width(1.dp)
-                        .height(48.dp)
-                        .background(
-                            color = MaterialTheme.colorScheme.onSurface,
-                            shape = RoundedCornerShape(50.dp)
-                        )
-                ) {
-
-                }
-            }
-            var backgroundModifier: Modifier = Modifier
-            if (index == selectedValue) {
-                backgroundModifier = when (index) {
-                    0 -> {
-                        Modifier.background(
-                            color = MaterialTheme.colorScheme.secondaryContainer,
-                            shape = RoundedCornerShape(
-                                bottomStartPercent = 50,
-                                topStartPercent = 50
-                            )
-                        )
-                    }
-
-                    values.size - 1 -> {
-                        Modifier.background(
-                            color = MaterialTheme.colorScheme.secondaryContainer,
-                            shape = RoundedCornerShape(bottomEndPercent = 50, topEndPercent = 50)
-                        )
-                    }
-
-                    else -> {
-                        Modifier.background(
-                            color = MaterialTheme.colorScheme.secondaryContainer,
-                        )
-                    }
-                }
-            }
-
-
+            val color =
+                if (index == selectedValue) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.surface
+            val onColor =
+                if (index == selectedValue) MaterialTheme.colorScheme.onSecondaryContainer else MaterialTheme.colorScheme.onSurface
+            val shape =
+                if (index == 0) RoundedCornerShape(topStartPercent = 50, bottomStartPercent = 50)
+                else if (index == values.size - 1) RoundedCornerShape(
+                    topEndPercent = 50,
+                    bottomEndPercent = 50
+                )
+                else RoundedCornerShape(0)
             Box(
-                contentAlignment = Alignment.Center,
-                modifier = backgroundModifier
-                    .weight(1f),
+                modifier = Modifier
+                    .height(height)
+                    .weight(1f)
+                    .clip(shape)
+                    .background(
+                        if (disabled[index]) disableColor
+                        else enableColor
+                    )
+                    .padding(
+                        if (index == 0) PaddingValues(
+                            top = borderSize,
+                            bottom = borderSize,
+                            start = borderSize,
+                            end = borderMediumSize
+                        )
+                        else if (index == values.size - 1) PaddingValues(
+                            top = borderSize,
+                            bottom = borderSize,
+                            start = borderMediumSize,
+                            end = borderSize
+                        )
+                        else PaddingValues(
+                            top = borderSize,
+                            bottom = borderSize,
+                            start = borderMediumSize,
+                            end = borderMediumSize
+                        )
+                    )
+                    .background(
+                        color,
+                        shape
+                    )
+                    .clickable(
+                        indication = null,
+                        interactionSource = MutableInteractionSource(),
+                    ){
+                        setSelectedValue(index)
+                    }
+                ,
+                contentAlignment = Alignment.Center
             ) {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxWidth()
-                        .height(48.dp)
-                        .clickable(indication = null, interactionSource = MutableInteractionSource()) {
-                            setSelectedValue(index)
-                        },
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 10.dp)
                 ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 10.dp)
-                    ) {
-                        if (index == selectedValue) {
-                            Icon(
-                                imageVector = Icons.Filled.Check,
-                                contentDescription = "",
-                                tint = onColor,
-                                modifier = Modifier
-                                    .padding(end = 10.dp)
-                                    .size(18.dp)
-                            )
-                        }
-                        Text(
-                            text = values[index],
-                            color = onColor
+                    if (index == selectedValue) {
+                        Icon(
+                            imageVector = Icons.Filled.Check,
+                            contentDescription = "",
+                            tint =
+                            if (disabled[index]) disableColor
+                            else onColor,
+                            modifier = Modifier
+                                .padding(end = 10.dp)
+                                .size(18.dp)
                         )
                     }
+                    Text(
+                        text = values[index],
+                        color =
+                        if (disabled[index]) disableColor
+                        else onColor
+                    )
                 }
             }
+            if (index != values.size - 1) Box(
+                modifier = Modifier
+                    .background(
+                        if (disabled[index] && disabled[index+1]) disableColor
+                        else enableColor
+                    )
+                    .height(height)
+                    .padding(horizontal = borderSize - .1.dp)
+            )
         }
     }
 }
